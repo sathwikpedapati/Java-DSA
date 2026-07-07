fc49500af6c1bd2efdbe63a38eefc083ffc0ea4d
sathwikpedapati <sathwikpedapati275@gmail.com>
2026-07-07 23:17:19 +0530

    added conditionals and loops

--- Basics/Calculator.java
+++ Basics/Calculator.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);  
+    Scanner input = new Scanner(System.in); 
+    // char op = input.next(); next or nextLine returns the string so that used charAt  method 
     char op = input.nextLine().charAt(0);
     int  m= input.nextInt();
     int  n= input.nextInt();
