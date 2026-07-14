0eda3a0fea77640dff431e30a742be3dfc3aee4d
sathwikpedapati <sathwikpedapati275@gmail.com>
2026-07-14 13:45:45 +0530

    added files

--- Patterns/DifferentTriangle.java
+++ Patterns/DifferentTriangle.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class DifferentTriangle {
     public static void main(String[] args) {
-        method(4);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        method(n);
     }
     static void method(int num){
         for(int i=1;i<=num;i++){
