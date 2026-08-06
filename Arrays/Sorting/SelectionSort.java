49c1269acfb962d2d71abea92a1cb1ece23410af
sathwikpedapati <sathwikpedapati275@gmail.com>
2026-08-06 16:29:41 +0530

    added selection sort

--- Arrays/Sorting/SelectionSort.java
+++ Arrays/Sorting/SelectionSort.java
@@ -9,10 +9,13 @@ public class SelectionSort {
     }
     static void sorting(int [] n){
         for(int i=0;i<n.length;i++){
+            // suppose the min value is assigned at i position
             int m=i;
             for(int j=i;j<n.length;j++){
+                // fetch the min value index
                 if(n[j]<n[m]) m=j;
             }
+            // Swap (a[min],a[i])
             int temp=n[m];
             n[m]=n[i];
             n[i]=temp;
